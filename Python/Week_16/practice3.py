9831cf62576d503ab9d04685933712c656242331
Jason <jescaleraalejandro@bostonk12.org>
2024-04-25 10:41:05 -0400

    Classwork for W13 - Competency - Git - Practice

---
+++ Python/Week_16/practice3.py
@@ -0,0 +1,18 @@
+# Jason Escalera
+# 4-25-24
+
+# This is how you create a empty list
+A = []
+
+# This is how you create a list with value
+B = [1,2,3]
+
+print(A)
+print(B)
+
+A.append('Glazinton')
+B.append(4)
+print(A)
+print(B)
+
+
