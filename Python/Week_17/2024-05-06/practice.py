f881fd0d6f8c6e16660d68a33141f6ea36929a31
Jason <jescaleraalejandro@bostonk12.org>
2024-05-06 10:43:49 -0400

    Classwork for W13 - Competency - Git - Practice

---
+++ Python/Week_17/2024-05-06/practice.py
@@ -0,0 +1,19 @@
+# Jason EScalera
+# 2024-05-06
+
+numbers=[]
+numbers+= (1)
+numbers+= (2)
+numbers+= (3)
+numbers+= (4)
+numbers+= (5)
+numbers+= (6)
+numbers+= (7)
+numbers+= (8)
+numbers+= (9)
+numbers+= (10)
+
+
+for b in range(1,001):
+numbers2+= [b]
+Number = []
