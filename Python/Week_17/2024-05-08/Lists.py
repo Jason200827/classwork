5a964b44206a4b800e72402756df88f2a4cedba5
Jason <jescaleraalejandro@bostonk12.org>
2024-05-08 10:04:50 -0400

    Classwork for W13 - Competency - Git - Practice

---
+++ Python/Week_17/2024-05-08/Lists.py
@@ -0,0 +1,11 @@
+# Jason Escalera
+# 2024-05-08
+
+# creating a empty list
+Lst = []
+
+# using  a for loop to add the number 1-100 to the list
+for a in range (1,101):
+    Lst.append(a)
+
+print(Lst)
