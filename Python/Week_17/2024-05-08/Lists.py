6ddddc22fff68fe959239f4bcb7f5edd233e0203
Jason <jescaleraalejandro@bostonk12.org>
2024-05-09 09:34:13 -0400

    Classwork for W13 - Competency - Git - Practice

--- Python/Week_17/2024-05-08/Lists.py
+++ Python/Week_17/2024-05-08/Lists.py
@@ -8,9 +8,23 @@ Lst = []
 for a in range (1,101):
     Lst.append(a)
 
+#printing the list Lst
 print(Lst)
-# creating a odd list
+
+# creating a empty list named  odd
 odd = []
+
+# using  a for loop to add the odd number 1-100 to the list odd
 for b in range (1,101,2):
     odd.append(b)
+
+# printing the list odd
 print(odd)
+
+even = []
+
+for c in range(2,101,2):
+    even.append(c)
+
+#printing the list even
+print(even)
